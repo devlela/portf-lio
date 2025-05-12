8a17468903f6dd072b266184dce545fcb1d56e15
Wesley Cavalcante Santos <wesley.santos2422@gmail.com>
2025-05-12 00:54:46 -0300

    Criar service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const cacheName = 'portfolio-cache-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/favicon.ico',
+  // Adicione outros recursos, como imagens e ícones, se necessário
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll(assets);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.map(key => {
+        if (key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
